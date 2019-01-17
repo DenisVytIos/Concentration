07b8e306110a0f5e8decaaaefb8dc3f006554cda
DenisVytIos <denvitios@gmail.com>
2019-01-17 16:42:11 +0200

    Repeated all the code from lecture 3

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -7,20 +7,51 @@
 //
 
 import Foundation
+    
 
 class Concentration {
     
-    var cards = [Card]()
+    private (set) var cards = [Card]()
     
+    private var indexOfOneAndOnlyFaceUpCard: Int?{
+        get{
+            var foundIndex: Int? = nil
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil{
+                        foundIndex = index
+                    } else{
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+            
+        }
+        set (newValue){
+            for index in cards.indices{
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     func chooseCard(at index:Int) {
-        if cards[index].isFaceUp{
-            cards[index].isFaceUp = false
-        }else{
-            cards[index].isFaceUp = true
+        assert(cards.indices.contains(index), "Concentration.choosesCard(at: \(index)): choosen index not in the cards")
+        if !cards[index].isMatched{
+            if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
+                if cards[matchIndex].identifier == cards[index].identifier{
+                    cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
+                }
+                cards[index].isFaceUp = true
+            }else{
+                indexOfOneAndOnlyFaceUpCard = index
+            }
         }
         
     }
+  
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you mast have at least one pair of cards")
         for _ in 1...numberOfPairsOfCards{
         let card = Card()
             cards += [card,card]
