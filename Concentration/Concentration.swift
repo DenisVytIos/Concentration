3ee34256e7217fb248507db376c407b1f1d56952
DenisVytIos <denvitios@gmail.com>
2019-01-28 12:53:59 +0200

    Repeated all the code from lecture 4

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -8,25 +8,19 @@
 
 import Foundation
     
-
-class Concentration {
+extension Collection{
+    var oneAndOnly: Element?{
+        return count == 1 ? first : nil
+    }
+    
+}
+struct Concentration {
     
     private (set) var cards = [Card]()
     
     private var indexOfOneAndOnlyFaceUpCard: Int?{
         get{
-            var foundIndex: Int? = nil
-            for index in cards.indices {
-                if cards[index].isFaceUp {
-                    if foundIndex == nil{
-                        foundIndex = index
-                    } else{
-                        return nil
-                    }
-                }
-            }
-            return foundIndex
-            
+            return cards.indices.filter{cards[$0].isFaceUp}.oneAndOnly
         }
         set (newValue){
             for index in cards.indices{
@@ -34,11 +28,11 @@ class Concentration {
             }
         }
     }
-    func chooseCard(at index:Int) {
+    mutating func chooseCard(at index:Int) {
         assert(cards.indices.contains(index), "Concentration.choosesCard(at: \(index)): choosen index not in the cards")
         if !cards[index].isMatched{
             if let matchIndex = indexOfOneAndOnlyFaceUpCard, matchIndex != index{
-                if cards[matchIndex].identifier == cards[index].identifier{
+                if cards[matchIndex] == cards[index]{
                     cards[matchIndex].isMatched = true
                     cards[index].isMatched = true
                 }
